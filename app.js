d373ea087047056b38aec628116ddb27e38e3d0c
lzbernardo <bernardo.lanza@gmail.com>
2017-03-30 02:46:14 -0300

    second commit

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cheerio = require('cheerio');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 1337;
+
+var url = "http://catedral.prefeitura.unicamp.br/cardapio.php";
+
+request(url, function(err, resp, body){
+  var $ = cheerio.load(body);
+  var whatsthis = $('.titulo');
+  var whatsthistext = whatsthis.text();
+
+  console.log(whatsthistext);
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {res.status(200).send('Hello World!'); });
 
-app.listen(port, function(){
-  console.log('Listening on port ' + port);
-});
-
 app.post('/hello', function(req, res, next){
   var username = req.body.user_name;
   var botPayLoad = {
-    text: 'Hey ' + username + ', I am Link, your link management assistant';
+    text: 'Hey ' + username + ', I am Link, your link management assistant'
   };
 
   if(username !== 'slackbot') {
@@ -23,4 +31,8 @@ app.post('/hello', function(req, res, next){
   } else {
     return res.status(200).end();
   }
+});
+
+app.listen(port, function(){
+  console.log('Listening on port ' + port);
 });
